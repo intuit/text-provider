6f7e84f58f8e0fe10052c2b21d6ff62286b195e1
MadaraUchiha-314 <rohithr31@gmail.com>
2020-03-29 19:41:23 +0530

    feat: adding commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+/**
+ * DISCLAIMER:
+ * This file helps enforce semantic commits. This is NECESSARY to allow for automated releases
+ * of a plugin. Removing or disabling it will break the release process, and is not recommended.
+ */
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+};
---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/**
+ * Prettier will read RC files, you can set your prettier rules here
+ */
+
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  printWidth: 100,
+  proseWrap: 'always',
+};
