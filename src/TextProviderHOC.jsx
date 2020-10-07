cdde1f76c8911db9b235edd17a7c247283959a73
cdodge1 <Conner_Dodge@intuit.com>
2020-10-07 10:16:39 -0600

    refactor: update TextProviderHOC to use context hook

--- src/TextProviderHOC.jsx
+++ src/TextProviderHOC.jsx
@@ -1,33 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { TextContext } from './TextProvider';
 
 const withTextProvider = (SomeComponent) => {
-  /* eslint-disable-line no-unused-vars */
-  class TextProviderHOC extends React.Component {
-    constructor(props) {
-      super(props);
-      this.getTextForKey = this.getTextForKey.bind(this);
-    }
+  function TextProviderHOC (props) {
+    const globalText = useContext(TextContext);
 
-    getTextForKey(key) {
-      const globalText = this.context;
+    function getTextForKey(key) {
       if (Object.prototype.hasOwnProperty.call(globalText, key)) {
         return globalText[key];
       }
       return '';
     }
 
-    render() {
-      return (
-        <SomeComponent
-          getTextForKey={this.getTextForKey}
-          {...this.props} /* eslint-disable-line react/jsx-props-no-spreading */
-        />
-      );
-    }
+    return (
+      <SomeComponent
+        getTextForKey={getTextForKey}
+        {...props} /* eslint-disable-line react/jsx-props-no-spreading */
+      />
+    );
   }
-
-  TextProviderHOC.contextType = TextContext;
 
   return TextProviderHOC;
 };
