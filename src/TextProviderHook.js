25d4365803bc670e13ff8d403ec2ee2b2d990af7
cdodge1 <Conner_Dodge@intuit.com>
2020-10-08 13:14:34 -0600

    fix: add text context

--- src/TextProviderHook.js
+++ src/TextProviderHook.js
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
+import { TextContext } from './TextProvider';
 
-function useTextProvider(context) {
+function useTextProvider(context = TextContext) {
   const globalText = useContext(context);
   return (key) => {
     if (Object.prototype.hasOwnProperty.call(globalText, key)) {
